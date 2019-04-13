ae2b48b693519884b457a2a350c210cfd0c131da
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-04-13 13:50:48 -0700

    connected updated db to inventory

--- server.js
+++ server.js
@@ -34,6 +34,19 @@ app.get('/inventory', (req, res) => {
 
 app.post('/icons-to-home', (req, res) => {
     console.log(req.body.si)
+    var selectedItem = req.body.si;
+    var currentUser = 1; //this should be req.sessions.user_id
+    con.query('DELETE FROM user_data WHERE user_id = 1', (err, results, fields) => {
+        if (err) throw err;
+        console.log('deleted all rows for current user...')
+    })
+
+    for (let i in selectedItem) {
+        con.query('INSERT INTO user_data (user_id, food_id, purchase_time) VALUES (?,?,?)', [1, selectedItem[i], '2019-04-10 12:00:00'], (err, results, fields) => {
+            if (err) throw err;
+            console.log(`Added food_id of ${selectedItem[i]} into user_data table, current user : ${currentUser}...`);
+        })
+    }
 });
 
 app.listen(3000, () => {
