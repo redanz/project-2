4f56412cf7bd6e297e526539047f3ae0b768d384
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-04-17 21:04:29 -0700

    added user id to add to home function

--- server.js
+++ server.js
@@ -29,6 +29,8 @@ app.get('/', (req, res) => {
     res.send('index')
 })
 
+var tempId = 3;
+
 app.post('/newUser', (req, res) => {
     bcrypt.genSalt(10, function (err, salt) {
         bcrypt.hash(req.body.password, salt, function (err, p_hash) {
@@ -40,13 +42,13 @@ app.post('/newUser', (req, res) => {
 })
 
 app.post('/newCustomIcon', (req, res) => {
-    con.query('INSERT INTO foods (food_name, expiry_time, custom_user_id) VALUES (?, ?, 1)', [req.body.foodName, req.body.expiration, 1], (err, results, fields) => {
+    con.query('INSERT INTO foods (food_name, expiry_time, custom_user_id) VALUES (?, ?, ?)', [req.body.foodName, req.body.expiration, tempId], (err, results, fields) => {
     })
     res.redirect('/inventory');
 })
 
 app.get('/inventory', (req, res) => {
-    con.query('SELECT id, food_name, custom_user_id FROM foods ORDER BY id ASC', (err, results, fields) => {
+    con.query('SELECT id, food_name, custom_user_id FROM foods WHERE custom_user_id = 0 OR custom_user_id = (?) ORDER BY id ASC;', tempId, (err, results, fields) => {
         res.render('pages/inventory', {
             foodInfo: results
         });
@@ -54,7 +56,7 @@ app.get('/inventory', (req, res) => {
 })
 
 app.get('/homedash', (req, res) => {
-    req.session.user_id = 1;
+    req.session.user_id = tempId;
     con.query('SELECT food_id, food_name, custom_user_id, expiry_time - DATEDIFF(CURDATE(), purchase_time) AS days_to_expiry FROM user_data LEFT JOIN foods ON foods.id = user_data.food_id WHERE user_id = ? ORDER BY days_to_expiry ASC', [req.session.user_id], (err, results, fields) => {
         if (err) throw err;
         res.render('pages/homedash', {
